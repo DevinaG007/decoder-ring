71a80fd5a3e65a33c66ae1674b27e0be83a25347
Devina Gillis <devinagillis0@gmail.com>
2023-07-24 19:04:22 -0700

    wrote code and tests for polybius function

--- src/polybius.js
+++ src/polybius.js
@@ -5,9 +5,62 @@
 
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
-
+  const polybiusKey = [
+    { number: 11, letter: "a" },
+    { number: 21, letter: "b" },
+    { number: 31, letter: "c" },
+    { number: 41, letter: "d" },
+    { number: 51, letter: "e" },
+    { number: 12, letter: "f" },
+    { number: 22, letter: "g" },
+    { number: 32, letter: "h" },
+    { number: 42, letter: ["i", "j"] },
+    { number: 52, letter: "k" },
+    { number: 13, letter: "l" },
+    { number: 23, letter: "m" },
+    { number: 33, letter: "n" },
+    { number: 43, letter: "o" },
+    { number: 53, letter: "p" },
+    { number: 14, letter: "q" },
+    { number: 24, letter: "r" },
+    { number: 34, letter: "s" },
+    { number: 44, letter: "t" },
+    { number: 54, letter: "u" },
+    { number: 15, letter: "v" },
+    { number: 25, letter: "w" },
+    { number: 35, letter: "x" },
+    { number: 45, letter: "y" },
+    { number: 55, letter: "x" },
+  ];
   function polybius(input, encode = true) {
-    // your solution code here
+    if (encode) {
+      let message = input.toLowerCase();
+      for (let i = 0; i < message.length; i++) {
+        polybiusKey.filter((alpha) => {
+          if (alpha.letter.includes(message[i])) {
+            message = message.replace(message[i], alpha.number);
+          }
+        });
+      }
+
+      return message;
+    }
+    if (!encode) {
+      if (input.replace(/ +/g, "").length % 2 !== 0) return false;
+      let message = input.replace(/ +/g, "");
+      let pair = [];
+      for (let i = 0; i < message.length; i++) {
+        pair.push(message[i] + message[(i += 1)]);
+      }
+      for (let i = 0; i < pair.length; i++) {
+        polybiusKey.forEach((alpha) => {
+          if (pair[i] == alpha.number) {
+            input = input.replace(pair[i], alpha.letter);
+          }
+        });
+      }
+      return input;
+    }
   }
 
   return {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,40 @@
 // Write your tests here!
+const polybiusModule = require("../src/polybius");
+const expect = require("chai").expect;
+
+describe("polybius(), my tests", () => {
+    it("should return a message with letters when a numerical code is given", () => {
+        const input = "12134325512434 112451 2224511144";
+        const actual = polybiusModule.polybius(input, encode = false);
+        const expected = "flowers are great"
+        expect(actual).to.equal(expected);
+    })
+  it("should return a string when encoding is set to true", () => {
+    const input = "hello world";
+    const actual = polybiusModule.polybius(input, encode = true);
+    expect(actual).to.be.a("string");
+  });
+  it("should return false if the inputted string has a odd length of characters, excluding spaces", () => {
+    const input = "3251131343 254324134";
+    const actual = polybiusModule.polybius(input, encode = false);
+    expect(actual).to.be.false;
+  });
+  it("should maintain spaces from the inputted string when decoding", () => {
+    const input = "3251131343 2543241341";
+    const actual = polybiusModule.polybius(input, (encode = false));
+    const expected = "hello world";
+    expect(actual).to.equal(expected);
+  });
+  it("should return both 'i' and 'j' when decoding the number 42", () => {
+    const input = "4432423352125413";
+    const actual = polybiusModule.polybius(input, encode = false);
+    expect(actual).to.include("i");
+    expect(actual).to.include("j");
+  });
+  it("should ignore capital letters when encoding", () => {
+    const input = "HelLo WorLD";
+    const actual = polybiusModule.polybius(input, encode = true);
+    const expected = "3251131343 2543241341";
+    expect(actual).to.equal(expected);
+  });
+});
